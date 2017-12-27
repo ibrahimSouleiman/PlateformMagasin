ce844a8a76310a48257dfe0231948ed74170a302
Medem <medem4991@gmail.com>
2017-12-27 12:28:52 +0100

    to do reduce stock and details in panier

--- src/M1/MagAppBundle/Controller/PanierController.php
+++ src/M1/MagAppBundle/Controller/PanierController.php
@@ -17,19 +17,65 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
 
 
 class PanierController extends Controller
 {
- public function listPanierAction()
+ public function listPanierAction(Request $request)
     {
-           //$em = $this->getDoctrine()->getManager();
-		   //$paniers = $em->getRepository(Paniers::class)->findPanierValider();
+           $Panier = new Paniers();
 
 		   $repository = $this->getDoctrine()->getRepository('M1MagAppBundle:Paniers');
 		   $paniers = $repository->findPanierValider();
 
-		return $this->render("M1MagAppBundle:Magasinier:List_Panier.html.twig", ['paniers'=> $paniers]);
+		//$produit = $em->getRepository(Produit::class)->find($id);
+      /*     $form = $this->createFormBuilder($Panier)
+                ->setMethod('POST')
+                ->add('save', SubmitType::class)
+                ->getForm();
+       // $form  = $this->get('form.factory')->create(PaniersType::class, $Panier);
+
+        if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
+             $em = $this->getDoctrine()->getManager();
+             $em->persist($paniers);
+             $em->flush();
+
+
+             return $this->redirectToRoute('saved_categorie', array('id' => $paniers->getId()));
+            }*/
    
+   return $this->render("M1MagAppBundle:Magasinier:List_Panier.html.twig",['paniers'=> $paniers]);
+  // return $this->render("M1MagAppBundle:Magasinier:List_Stock.html.twig", ['produits'=> $produits]);
     }
+
+
+    public function validerPanierAction($id)
+    {
+    	 //$Panier = new Paniers();
+     
+         $em = $this->getDoctrine()->getManager();
+		 $panier = $em->getRepository(Paniers::class)->find($id);
+
+		 // verifier le stock
+
+		$panier->setEtat("traité");
+
+		 $em->persist($panier);
+         $em->flush();
+
+  		return $this->redirectToRoute('list_panier');
+    }
+
+    public function detailsPanierAction($id)
+    {
+    	$em = $this->getDoctrine()->getManager();
+		$panier = $em->getRepository(Paniers::class)->find($id);
+
+		return $this->render("M1MagAppBundle:Magasinier:Cmd_Panier.html.twig",['panier'=> $panier]);
+
+    }
+
 }
